c50cc948fe03677a22a866c197d2fb57dfcddcea
Lanksy <104015308+lankybox02@users.noreply.github.com>
2022-06-21 14:48:05 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Wasteof Bot Starter Kit
+A simple starter kit for creating a wasteof.money bot using `wasteof-client`.
+### Getting started
+To get started, clone this repository on replit.com or a programming IDE that supports node.js
+
+Go to the repository shell and type `npm i wasteof-client`. Then, change the `username` and `password` variables to match the bot account.
+
+**Note**: It is recommended to use your env to store your password!
+
+### Handling errors
+If you received any errors that you don't understand, please comment on my wall:
+
+[Beta](https://beta.wasteof.money/users/lankybox01/wall) - [Prod](https://wasteof.money/users/lankybox01/wall)
---
+++ index.js
@@ -0,0 +1,33 @@
+import { Wasteof2Auth, Wasteof2 } from 'wasteof-client'
+
+let username = 'jeffalo';
+let password = 'bacon';
+const wasteof = new Wasteof2Auth(username, password);
+const wastatic = new Wasteof2();
+
+await wasteof.login()
+wasteof.listen(async (event) => {
+    if (event.type == 'updateMessageCount') {
+	await wastatic.getWallComments(username, 0)
+	    .then(data => {
+		replyToComment(data.comments[0]);
+	    });
+    }
+});
+
+async function replyToComment(comment) {
+  let postername = comment.poster.name; // username the one leaving the comment
+  let posterid = comment.poster.id; // id of the one leaving the comment
+  let content = comment.content; // content of the comment
+  let commentid = comment._id; // id of the comment
+  let time = comment.time; // timestamp of the comment
+  let responded = false; // if the bot already responded
+
+ 	await wastatic.getRepliesToComment(commentid, 0)
+  .then(data => {
+    if (data[0].poster.name == username) responded = true;
+  })
+
+  /* DO STUFF HERE */
+  if (responded) return; // if the bot already responded to the comment, stop the function
+}
